7d54f55b0076a15ca1b46a9ab8a184775ac2115d
Nicolas Albert <nicolas.albert@gmail.com>
2017-01-20 18:00:19 +0100

    Auto configure administration password environment variables provided

--- 7.4/7.4.3/web-connector/docker-entrypoint.sh
+++ 7.4/7.4.3/web-connector/docker-entrypoint.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
 
 if [ "$1" = "convertigo" ]; then
-
+    
+    WEB_INF=$CATALINA_HOME/webapps/convertigo/WEB-INF
+    
+    ## function used to cipher passwords
+    
+    toHash() {
+        jexec $WEB_INF/lib/rhino-*.jar -e "print(new java.lang.String('$1').hashCode())"
+    }
+     
+        
+    ## if needed, force the admin and testplatform accounts
+    
+    if [ "$CONVERTIGO_ADMIN_USER" != "" ]; then
+        export JAVA_OPTS="-Dconvertigo.engine.admin.username=$CONVERTIGO_ADMIN_USER $JAVA_OPTS"
+        unset CONVERTIGO_ADMIN_USER
+    fi
+    
+    if [ "$CONVERTIGO_ADMIN_PASSWORD" != "" ]; then
+        export JAVA_OPTS="-Dconvertigo.engine.admin.password=$(toHash $CONVERTIGO_ADMIN_PASSWORD) $JAVA_OPTS"
+        unset CONVERTIGO_ADMIN_PASSWORD
+    fi
+    
+    if [ "$CONVERTIGO_TESTPLATFORM_USER" != "" ]; then
+        export JAVA_OPTS="-Dconvertigo.engine.testplatform.username=$CONVERTIGO_TESTPLATFORM_USER $JAVA_OPTS"
+        unset CONVERTIGO_TESTPLATFORM_USER
+    fi
+    
+    if [ "$CONVERTIGO_TESTPLATFORM_PASSWORD" != "" ]; then
+        export JAVA_OPTS="-Dconvertigo.engine.testplatform.password=$(toHash $CONVERTIGO_TESTPLATFORM_PASSWORD) $JAVA_OPTS"
+        unset CONVERTIGO_TESTPLATFORM_PASSWORD
+    fi
+    
+    
     ## add the linked couchdb container as the fullsync couchdb
     
-    if [ "$(getent hosts couchdb)" != "" ];then
+    if [ "$(getent hosts couchdb)" != "" ]; then
         export JAVA_OPTS="-Dconvertigo.engine.fullsync.couch.url=http://couchdb:5984 $JAVA_OPTS"
     fi
     
     
     ## add custom jar or class to the convertigo server
     
-    cp -r /workspace/lib/* $CATALINA_HOME/webapps/convertigo/WEB-INF/lib/ 2>/dev/null
-    cp -r /workspace/classes/* $CATALINA_HOME/webapps/convertigo/WEB-INF/classes/ 2>/dev/null
+    cp -r /workspace/lib/* $WEB_INF/lib/ 2>/dev/null
+    cp -r /workspace/classes/* $WEB_INF/classes/ 2>/dev/null
     
     
     ## default common JAVA_OPTS, can be extended with "docker run -e JAVA_OPTS=-custom" 
@@ -29,7 +61,7 @@ if [ "$1" = "convertigo" ]; then
     ## the web-connector version need can use an existing DISPLAY or declare one
     ## the mbaas version need to be headless and remove the DISPLAY variable
     
-    if [ -d $CATALINA_HOME/webapps/convertigo/WEB-INF/xvnc ]; then
+    if [ -d $WEB_INF/xvnc ]; then
         export DISPLAY=${DISPLAY:-:0}
     else
         unset DISPLAY
